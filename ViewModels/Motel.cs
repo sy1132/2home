595ce6a9f10feaad6e34aff4501c33d99dd829a5
sy1132 <2224802010369@student.tdmu.edu.vn>
2024-09-22 21:03:27 +0700

    22/09

---
+++ ViewModels/Motel.cs
@@ -0,0 +1,12 @@
+﻿namespace _2home.ViewModels
+{
+    public class Motel
+    {
+        public int ID_user { get; set; }
+        public string MotelName { get; set; }
+        public string Location { get; set; }
+        public decimal Price { get; set; }
+        public string is_available { get; set; } 
+        public string ImgLink { get; set; }
+    }
+}
