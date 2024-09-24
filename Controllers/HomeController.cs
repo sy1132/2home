b919d26ed8b420a56307da52c8aa866f2ff94ede
sy1132 <2224802010369@student.tdmu.edu.vn>
2024-09-24 20:57:53 +0700

    2409

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -16,6 +16,8 @@ using System.Web.Helpers;
 using System.Security.Principal;
 using Microsoft.Ajax.Utilities;
 using System.Security.Policy;
+using System.Text.RegularExpressions;
+using System.Threading;
 
 namespace _2home.Controllers
 {
@@ -31,16 +33,23 @@ namespace _2home.Controllers
             var query = from img in db.imgs
                         join motel in db.Motels on img.ID_user equals motel.ID_user
                         where motel.is_available == "Còn trống"
-                        select new index_Viewmodel
+                        group img by new
                         {
-                            ImgLink = img.Link,
-                            MotelName = motel.Name_motel,
-                            Location = motel.location,
-                            Price = motel.price,
-                            is_available = motel.is_available,
-                            ID_user=motel.ID_user,
-
-                        };
+                            motel.Name_motel,
+                            motel.location,
+                            motel.price,
+                            motel.is_available,
+                            motel.ID_user
+                        } into grouped
+                select new index_Viewmodel
+                {
+                    ImgLink = grouped.Select(g => g.Link).FirstOrDefault(),
+                    MotelName = grouped.Key.Name_motel,
+                    Location = grouped.Key.location,
+                    Price = grouped.Key.price,
+                    is_available = grouped.Key.is_available,
+                    ID_user = grouped.Key.ID_user
+                };
             if (!String.IsNullOrEmpty(searchString))
                 query = query.Where(b => b.MotelName.Contains(searchString));
             ViewBag.Page = page;
@@ -193,7 +202,7 @@ namespace _2home.Controllers
             return View();
 
         }
-        public ActionResult Manager_DK(int? size, int? page)
+        public ActionResult Manager_DK(int? size, int? page, string fullname, string motelID, string userrole)
 
         {
             var query = db.users.Select(u => new User
@@ -208,6 +217,20 @@ namespace _2home.Controllers
                 userrole=u.userrole,
                 gender = u.gender
             });
+            if (!String.IsNullOrEmpty(fullname))
+            {
+                query = query.Where(u => u.fullname.Contains(fullname));
+            }
+
+            if (!String.IsNullOrEmpty(motelID))
+            {
+                query = query.Where(u => u.MotelID.ToString() == motelID);
+            }
+
+            if (!String.IsNullOrEmpty(userrole))
+            {
+                query = query.Where(u => u.userrole == userrole);
+            }
 
             ViewBag.Page = page;
             List<SelectListItem> items = new List<SelectListItem>();
@@ -232,23 +255,66 @@ namespace _2home.Controllers
             return View(model.ToPagedList(pageNumber, pageSize));
 
         }
-        public ActionResult rental_management(int? size, int? page)
+        [HttpPost]
+        public ActionResult UpdateUserRole(int ID_user, string userrole)
+        {
+            
+                using (var context = new DataClasses1DataContext())
+                {
+                    var role = context.users.FirstOrDefault(m => m.ID_user == ID_user);
+                    if (role != null)
+                    {
+                        role.userrole = userrole;
+                        context.SubmitChanges();
+                    }
+                }
+                return RedirectToAction("Manager_DK");
+        }
+
+        public ActionResult rental_management(int? size, int? page, string MotelName, string Location, decimal? Price1, decimal? Price2, string is_available)
 
         {
-
-
             ViewBag.message = "Quản lý đăng ký cho thuê";
             var query = from img in db.imgs
                         join motel in db.Motels on img.ID_user equals motel.ID_user
+                        group img by new
+                        {
+                            motel.Name_motel,
+                            motel.location,
+                            motel.price,
+                            motel.is_available,
+                            motel.ID_user
+                        } into grouped
                         select new index_Viewmodel
-                        { 
-                            ImgLink = img.Link,
-                            MotelName = motel.Name_motel,
-                            Location = motel.location,
-                            Price = motel.price,
-                            is_available = motel.is_available,
-                            ID_user=motel.ID_user,
+                        {
+                            ImgLink = grouped.Select(g => g.Link).FirstOrDefault(),
+                            MotelName = grouped.Key.Name_motel,
+                            Location = grouped.Key.location,
+                            Price = grouped.Key.price,
+                            is_available = grouped.Key.is_available,
+                            ID_user = grouped.Key.ID_user
                         };
+            if (!string.IsNullOrEmpty(MotelName))
+            {
+                query = query.Where(m => m.MotelName.Contains(MotelName));
+            }
+
+            if (!string.IsNullOrEmpty(Location))
+            {
+                query = query.Where(m => m.Location.Contains(Location));
+            }
+            if (Price1.HasValue)
+            {
+                query = query.Where(m => m.Price >= Price1.Value);
+            }
+            if (Price2.HasValue)
+            {
+                query = query.Where(m => m.Price <= Price2.Value);
+            }
+            if (!String.IsNullOrEmpty(is_available))
+            {
+                query = query.Where(u => u.is_available == is_available);
+            }
             ViewBag.Page = page;
             List<SelectListItem> items = new List<SelectListItem>();
             items.Add(new SelectListItem { Text = "10", Value = "10" });
--- Models/DataClasses1.designer.cs
+++ Models/DataClasses1.designer.cs
@@ -53,7 +53,7 @@ namespace _2home.Models
 			OnCreated();
 		}
         public DataClasses1DataContext() :
-        base(global::System.Configuration.ConfigurationManager.ConnectionStrings["secondhomeConnectionString2"].ConnectionString, mappingSource)
+        base(global::System.Configuration.ConfigurationManager.ConnectionStrings["secondhomeConnectionString"].ConnectionString, mappingSource)
         {
 
         }
