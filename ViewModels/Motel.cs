79c3194daf14f3aa8e7b4204791e053ee3b09313
sy1132 <2224802010369@student.tdmu.edu.vn>
2024-10-19 19:12:55 +0700

    Revert "1810"
    This reverts commit b815c07db1fb0fd83e905fdf29d571c8b38bf3d7.

--- ViewModels/Motel.cs
+++ ViewModels/Motel.cs
@@ -3,13 +3,9 @@
     public class Motel
     {
         public int ID_user { get; set; }
-        public int Motel_ID { get; set; }
-
         public string MotelName { get; set; }
         public string Location { get; set; }
-        public decimal? Price { get; set; }
-        public decimal? Debt { get; set; }
-
+        public decimal Price { get; set; }
         public string is_available { get; set; } 
         public string ImgLink { get; set; }
         public string Details { get; set; }
